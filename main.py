a4cbfb7573b3a6de81c3b7107c592dddedcfa53c
Semen Gorunovich <sg@digift.ru>
2026-09-13 14:35:37 +0300

    /wallets/{name}

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
 from fastapi.responses import Response
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
+
+BALANCE = {}
+
+@app.get("/balance")
+def get_balance(wallet_name: str | None = None):
+    if wallet_name is None:
+        return {"total_balance": sum(BALANCE.values())}
+    if wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet `{wallet_name}` not found"
+        )
+    return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+@app.post("/wallets/{name}")
+def receive_money(name: str, amount: int):
+    if name not in BALANCE:
+        BALANCE[name] = 0
+    BALANCE[name] += amount
+
+    return {
+        "message": f"Added {amount} to {name}",
+        "wallet": name,
+        "new_balance": BALANCE[name]
+    }
+
 
 @app.get("/health")
 def health_check():
